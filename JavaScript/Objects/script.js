aa9698b795362ea93a0629ecf20103c52b9ebe33
V_Kurochkin <v.kurochkin1269@gmail.com>
2023-12-11 21:17:34 +0700

    Create JS objects

---
+++ JavaScript/Objects/script.js
@@ -0,0 +1,58 @@
+(function () {
+    function getCountriesByMaxCitiesAmount(countries) {
+        let maxCitiesAmount = countries.reduce((max, country) => {
+            return max > country.cities.length ? max : country.cities.length;
+        }, -1);
+
+        return countries.filter(country => country.cities.length === maxCitiesAmount);
+    }
+
+    function generateObjectCountries(countries){
+        let resultCountries = {};
+
+        countries.forEach((item) => {
+            resultCountries[item.name] = item.cities.reduce((sum, city) => sum + city.population, 0);
+        });
+
+        return resultCountries;
+    }
+
+    let countries = [
+        {
+            name: "Италия",
+            cities: [{name: "Рим", population: 1935609},
+                {name: "Милан", population: 1095830},
+                {name: "Неаполь", population: 745170},
+                {name: "Палермо", population: 238705}]
+        },
+        {
+            name: "Венесуэла",
+            cities: [{name: "Каракас", population: 3300000},
+                {name: "Валенсия", population: 830000}]
+        },
+        {
+            name: "Куба",
+            cities: [{name: "Гавана", population: 2135498},
+                {name: "Камагуэй", population: 415000},
+                {name: "Гуантанамо", population: 248893}]
+        },
+        {
+            name: "Ватикан",
+            cities: [{name: "Ватикан", population: 825}]
+        },
+        {
+            name: "Польша",
+            cities: [{name: "Краков", population: 1497600},
+                {name: "Варшава", population: 2327300},
+                {name: "Познань", population: 438600},
+                {name: "Люблин", population: 489800}]
+        }
+    ];
+
+    console.log(countries);
+    console.log("Страны с максимальным количеством городов:");
+    console.log(getCountriesByMaxCitiesAmount(countries));
+
+    console.log("Новый обьект ключ - Страна, значение - численность:");
+    console.log(generateObjectCountries(countries));
+})();
